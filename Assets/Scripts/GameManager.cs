50c36749ad7965a17b62cab7c8c4dfa2aa0134f8
caden hl <62642278+khaitick@users.noreply.github.com>
2021-07-23 21:09:33 +0800

    Initialize Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreUI;
+    public bool spawn;
+    public bool gameOver;
+    [Range(0,10)]
+    public int spawnWave;
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (!gameOver)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            GameObject target = Instantiate(targetPrefabs[index]);
+        }
+    }
+
+
+    public void UpdateScore(int value)
+    {
+        score += value;
+        scoreUI.text = score.ToString();
+    }
+
+    
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    public ParticleSystem ps;
+    public int point;
+
+    private Rigidbody rb;
+    private GameManager gm;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        transform.position = new Vector3(Random.Range(-4, 4), -4);
+        transform.localScale *= Random.Range(0.5f, 1f);
+        rb.AddForce(Vector3.up * Random.Range(8, 14), ForceMode.Impulse);
+        rb.AddTorque(10, 10, 10, ForceMode.Impulse);
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < -10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        gm.UpdateScore(point);
+        Instantiate(ps, transform.position, ps.transform.rotation);
+        Destroy(gameObject);
+    }
+
+}
